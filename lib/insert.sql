a77662dfe67b16171d081d8ade1455968eac7877
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-27 21:22:04 +0300

    completed organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Mr. Chocolate", 10, "M", "chocolate","angry", 1);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Rowdy", 4, "M", "brown", "friendly", 1);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Tabitha", 15, "F", "blue", "moody", 0);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Sergeant Brown", 8, "M", "white", "angry", 1);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Melissa", 12, "F", "grey", "shy", 0);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Grinch", 9, "M", "green", "bully", 1);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Wendy", 6, "F", "beige", "cool", 0);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES(NULL, 4, "F", "jungle green", "dead", 0);
